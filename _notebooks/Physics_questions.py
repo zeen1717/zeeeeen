60fdac1fa008c76ff9049f89852728392d540970
zeen <zeen6808005293@gmail.com>
2022-11-10 09:26:50 -0800

    correction

---
+++ _notebooks/Physics_questions.py
@@ -0,0 +1,100 @@
+import random
+
+Physics_data = []
+Physics_list = [
+    "question_1:solution_1",
+    "question_2:solution_2",
+    "question_3:solution_3",
+    "question_4:solution_4",
+    "question_5:solution_5",
+    "question_6:solution_6",
+    "question_7:solution_7",
+    "question_8:solution_8",
+    "question_9:solution_9",
+    "question_10:solution_10"
+]
+
+# Initialize jokes
+def initJokes():
+    # setup jokes into a dictionary with id, joke, haha, boohoo
+    item_id = 0
+    for item in joke_list:
+        jokes_data.append({"id": item_id, "joke": item, "haha": 0, "boohoo": 0})
+        item_id += 1
+    # prime some haha responses
+    for i in range(10):
+        id = getRandomJoke()['id']
+        addJokeHaHa(id)
+    # prime some haha responses
+    for i in range(5):
+        id = getRandomJoke()['id']
+        addJokeBooHoo(id)
+        
+# Return all jokes from jokes_data
+def getJokes():
+    return(jokes_data)
+
+# Joke getter
+def getJoke(id):
+    return(jokes_data[id])
+
+# Return random joke from jokes_data
+def getRandomJoke():
+    return(random.choice(jokes_data))
+
+# Liked joke
+def favoriteJoke():
+    best = 0
+    bestID = -1
+    for joke in getJokes():
+        if joke['haha'] > best:
+            best = joke['haha']
+            bestID = joke['id']
+    return jokes_data[bestID]
+    
+# Jeered joke
+def jeeredJoke():
+    worst = 0
+    worstID = -1
+    for joke in getJokes():
+        if joke['boohoo'] > worst:
+            worst = joke['boohoo']
+            worstID = joke['id']
+    return jokes_data[worstID]
+
+# Add to haha for requested id
+def addJokeHaHa(id):
+    jokes_data[id]['haha'] = jokes_data[id]['haha'] + 1
+    return jokes_data[id]['haha']
+
+# Add to boohoo for requested id
+def addJokeBooHoo(id):
+    jokes_data[id]['boohoo'] = jokes_data[id]['boohoo'] + 1
+    return jokes_data[id]['boohoo']
+
+# Pretty Print joke
+def printJoke(joke):
+    print(joke['id'], joke['joke'], "\n", "haha:", joke['haha'], "\n", "boohoo:", joke['boohoo'], "\n")
+
+# Number of jokes
+def countJokes():
+    return len(jokes_data)
+
+# Test Joke Model
+if __name__ == "__main__": 
+    initJokes()  # initialize jokes
+    
+    # Most likes and most jeered
+    best = favoriteJoke()
+    print("Most liked", best['haha'])
+    printJoke(best)
+    worst = jeeredJoke()
+    print("Most jeered", worst['boohoo'])
+    printJoke(worst)
+    
+    # Random joke
+    print("Random joke")
+    printJoke(getRandomJoke())
+    
+    # Count of Jokes
+    print("Jokes Count: " + str(countJokes()))
